4765c4bdb2edb301c4d72225c36c0849194c9660
amc960 <109912892+amc960@users.noreply.github.com>
2022-09-30 18:05:04 -0400

    Update BERT for Sentment Analysis.py

--- Project/BERT for Sentment Analysis.py
+++ Project/BERT for Sentment Analysis.py
@@ -9,6 +9,8 @@ Created on Sat Sep 24 19:19:03 2022
 ## Sentiment Analysis of Tweets using BERT - Thinking Neuron
 ## https://thinkingneuron.com/sentiment-analysis-of-tweets-using-bert/
 
+# Import Packages
+
 import pandas as pd
 import json
 import gzip
@@ -21,16 +23,14 @@ import matplotlib.pyplot as plt
 
 from transformers import BertTokenizer, TFBertForSequenceClassification
 from transformers import InputExample, InputFeatures
-
 import tensorflow as tf
-
-# importing the pipeline module
 from transformers import pipeline
 
 
-# This is Amanda's attempt at BERT Sentiment Analysis
+# BERT Sentiment analysis
 
 # See current working directory
+
 os.getcwd()
 
 
@@ -40,131 +40,154 @@ os.chdir('C:\\users\\cchee\\OneDrive\\Desktop')
 
 #read the file
 
-clean_data = pd.read_parquet('team8_initial_clean.parquet.gz', engine ='pyarrow')
+clean_data = pd.read_parquet('new_presentiment_whole.parquet.gz', engine ='pyarrow')
 
-#Check the size of the data
+print(clean_data.index) #Check the size of the data
 
-print(clean_data.index)
+# make sure the raw review text has less than 512 tokens for BERT to run
 
-#Set a seed and sample a random 250,000 rows of data
+clean_data['smallReview'] = clean_data['original_text'].apply(lambda x: len(x.split()))
 
-amanda_sample = clean_data.sample(n=200000, random_state= 12345)
 
-print(amanda_sample.index)
+#Set a seed and sample a random 200,000 rows of data
+
+BERT_sample = clean_data.sample(n=200000, random_state= 321)
+
+print(BERT_sample.index) # double check the size of the sample
+
+BERT_sample.to_csv('BERTsample_data.csv') # store to working directory
+
+
+# Make sure the summary and full text reviews are strings
+
+BERT_sample['original_summary'] = BERT_sample['original_summary'].astype(str)
+BERT_sample['original_text'] = BERT_sample['original_text'].astype(str)
 
 # Build the pretrained BERT model with sequence classifier and tokenizer
 
-# Downloading the sentiment analysis model
-SentimentClassifier = pipeline("sentiment-analysis")
+SentimentClassifier = pipeline("sentiment-analysis") # Downloading the sentiment analysis model
 
 
-# Calling the sentiment analysis function for 3 sentences - test this.
-SentimentClassifier(["I hope we get all these concepts! Its killing the neurons of our brain",
-                     "We had a nice experience in this trip",
-                     "Houston we have a problem"
+# Calling the sentiment analysis function on some test sentences
+
+SentimentClassifier(["We had a nice experience in this trip",
+                     "Houston we have a problem", 
+                     "I hate this store"
                       ])
 
 # Try the analysis on the Amazon review data
 
-# Defining a function to call for the whole dataframe
+# Defining a function to call the label for the whole dataframe
+
 def FunctionBERTSentiment(inpText):
   return(SentimentClassifier(inpText)[0]['label'])
  
-# Calling the function - just test it
-FunctionBERTSentiment(inpText="I'm not sure this worked right")
+# Calling the label function - just test it
+
+FunctionBERTSentiment(inpText="This is fun!")
+
+# Defining a function to call the score for the whole dataframe
+
+def FunctionBERTSentimentScore(inpText):
+  return(SentimentClassifier(inpText)[0]['score'])
+
+# Calling the score function - just test it
+
+FunctionBERTSentimentScore(inpText="This is fun!")
 
 
-# Calling BERT based sentiment score function for every review
-amanda_sample['Sentiment']=amanda_sample['reviewText'].apply(FunctionBERTSentiment)
-amanda_sample.head(200)
+# Calling BERT based sentiment label function for every full review
 
-amanda_sample.to_csv('BERTsample_output.csv')
+BERT_sample['FullSentiment']=BERT_sample['original_text'].apply(FunctionBERTSentiment)
+BERT_sample.head(200)
 
-# Plot number of reviews by subcategory and sentiment
-pivot = pd.pivot_table(amanda_sample, values = 'asin', index = 'sub_category', columns = 'Sentiment', aggfunc='count')
+BERT_sample.to_csv('BERTfull_sample_data.csv') # store to working directory
+
+# Calling BERT based sentiment label function for every summary review
+
+BERT_sample['SummSentiment']=BERT_sample['original_summary'].apply(FunctionBERTSentiment)
+BERT_sample.head(200)
+
+BERT_sample.to_csv('BERTsentiments_all.csv') # Write to working directory
+
+# Calling BERT based sentiment score function for every full review
+
+BERT_sample['FullScore']=BERT_sample['original_text'].apply(FunctionBERTSentimentScore)
+BERT_sample.head(200)
+
+BERT_sample.to_csv('BERTsentiments_fullScore.csv') # Write to working directory
+
+# Calling BERT based sentiment score function for every full review
+
+BERT_sample['SummScore']=BERT_sample['original_summary'].apply(FunctionBERTSentimentScore)
+BERT_sample.head(200)
+
+BERT_sample.to_csv('BERTsentiment_all.csv')  # write to working directory
+
+# Create bar charts of reviews by sentiment, product category, and number of stars
+
+# Plot number of reviews by subcategory and sentiment on full review
+
+pivot = pd.pivot_table(BERT_sample, values = 'asin', index = 'sub_category', columns = 'FullSentiment', aggfunc='count')
 pivot = pivot.reset_index()
 pivot.plot.bar(x = 'sub_category', rot = 50)
 plt.xlabel("Sub Category")
 plt.ylabel("Number of Reviews")
-plt.title("Reviews by Sub Category and Sentiments")
+plt.title("Full Text Reviews by Sub Category and Sentiment")
 plt.legend(title="Sentiment", loc='best', fontsize='small', fancybox=True)
 current_values = plt.gca().get_yticks()
 plt.show()
 
-# Plot number of reviews by Star rating and sentiment
-pivot = pd.pivot_table(amanda_sample, values = 'asin', index = 'overall', columns = 'Sentiment', aggfunc='count')
+# Plot number of full reviews by Star rating and sentiment
+
+pivot = pd.pivot_table(BERT_sample, values = 'asin', index = 'overall', columns = 'FullSentiment', aggfunc='count')
 pivot = pivot.reset_index()
 pivot.plot.bar(x = 'overall', rot = 50)
 plt.xlabel("Star Rating")
 plt.ylabel("Number of Reviews")
-plt.title("Reviews by Overall Star Rating and Sentiments")
+plt.title("Full Text Reviews by Overall Star Rating and Sentiment")
 plt.legend(title="Sentiment", loc='best', fontsize='small', fancybox=True)
 current_values = plt.gca().get_yticks()
 plt.show()
 
-############### what needs to be done to figure out accuracy ###########################
+# Plot number of summary reviews by subcategory and sentiment on 
 
-### https://discuss.pytorch.org/t/f1-score-in-pytorch-for-evaluation-of-the-bert/144382
+pivot = pd.pivot_table(BERT_sample, values = 'asin', index = 'sub_category', columns = 'SummSentiment', aggfunc='count')
+pivot = pivot.reset_index()
+pivot.plot.bar(x = 'sub_category', rot = 50)
+plt.xlabel("Sub Category")
+plt.ylabel("Number of Reviews")
+plt.title("Summary Reviews by Sub Category and Sentiment")
+plt.legend(title="Sentiment", loc='best', fontsize='small', fancybox=True)
+current_values = plt.gca().get_yticks()
+plt.show()
 
-def evaluate(model, val_dataloader):
-    """
-    After the completion of each training epoch, measure the model's performance
-    on our validation set.
-    """
-    # Put the model into the evaluation mode. The dropout layers are disabled during
-    # the test time.
-    model.eval()
+# Plot number of summary reviews by Star rating and sentiment
 
-    # Tracking variables
-    val_accuracy = []
-    val_loss = []
-    f1_weighted = []
+pivot = pd.pivot_table(BERT_sample, values = 'asin', index = 'overall', columns = 'SummSentiment', aggfunc='count')
+pivot = pivot.reset_index()
+pivot.plot.bar(x = 'overall', rot = 50)
+plt.xlabel("Star Rating")
+plt.ylabel("Number of Reviews")
+plt.title("Summary Reviews by Overall Star Rating and Sentiment")
+plt.legend(title="Sentiment", loc='best', fontsize='small', fancybox=True)
+current_values = plt.gca().get_yticks()
+plt.show()
 
-    # For each batch in our validation set...
-    for batch in val_dataloader:
-        # Load batch to GPU
-        b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+# Create the box plots of scores for full text reviews
 
-        # Compute logits
-        with torch.no_grad():
-            logits = model(b_input_ids, b_attn_mask)
-
-        # Compute loss
-        loss = loss_fn(logits, b_labels)
-        val_loss.append(loss.item())
-
-        # Get the predictions
-        preds = torch.argmax(logits, dim=1).flatten()
-
-        # Calculate the accuracy rate
-        accuracy = (preds == b_labels).cpu().numpy().mean() * 100
-        val_accuracy.append(accuracy)
-
-        # Calculate the f1 weighted score
-        f1_metric = F1Score('weighted') 
-        f1_weighted = f1_metric(preds, b_labels)
-
-    # Compute the average accuracy and loss over the validation set.
-    val_loss = np.mean(val_loss)
-    val_accuracy = np.mean(val_accuracy)
-    f1_weighted = np.mean(f1_weighted)
-
-    return val_loss, val_accuracy, f1_weighted 
+plt.boxplot(BERT_sample['FullScore'])
+plt.ylabel("Sentiment Score")
+plt.tick_params(right = False, labelbottom= False, bottom = False)
+plt.title("Sentiment Score for Full Text Reviews") # Change title to match dataset
+plt.show()
 
 
+# Create the box plots of scores for summary text reviews
 
+plt.boxplot(BERT_sample['SummScore'])
+plt.ylabel("Sentiment Score")
+plt.tick_params(right = False, labelbottom= False, bottom = False)
+plt.title("Sentiment Score for Summary Reviews") # Change title to match dataset
+plt.show()
 
-
-# Alex's funtion - can we use something like this???
-
-def getAnalysis(score):
-    if score < -.6:
-        return "Negative"
-    elif score >= -.6 and score < -.2:
-        return "Slightly Negative"
-    elif score >= -.2 and score < .2:
-        return "Neutral"
-    elif score >= .2 and score < .6:
-        return "Slightly Positive"
-    else:
-        return "Positive"
